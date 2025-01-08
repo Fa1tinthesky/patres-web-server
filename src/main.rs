7c685036f9c111cf0ab6f61f55c8c52c0b0b91ab
Fa1tinthesky <fate.public@gmail.com>
2025-01-08 12:21:47 +0500

    Added javascript to index.html to test basic functionality

--- src/main.rs
+++ src/main.rs
@@ -21,15 +21,29 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {   
     // &stream: bytes
+    let ws_root = "./src/html";
+    let db_root = "./src/db_html";
+
+    let url_root = "http://127.0.0.1:5050";
+
     let buf_reader = BufReader::new(&stream); 
     let mut buf_writer = BufWriter::new(&stream);
 
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
-    // println!("Request: {http_request:#?}");
+    let http_request: Vec<_>= buf_reader
+            .lines()
+            .map(|result| result.unwrap())
+            .take_while(|x| !x.is_empty())
+            .collect();
 
-    if (request_line == "GET / HTTP/1.1") {
+    let request_line = &http_request[0];
+    let request_line_split: Vec<_> = request_line.split(' ').collect();
+
+    println!("Request: {http_request:#?}");
+    println!("First line: {request_line:#?}");
+
+    if request_line == "GET / HTTP/1.1" {
         // buf_writer.write(b"<p>Hello there, world!</p>").unwrap();
-        let contents = fs::read_to_string("./src/html/index.html").unwrap();
+        let contents = fs::read_to_string(format!("{}{}", ws_root, "/index.html")).unwrap();
         let status_line = "HTTP/1.1 200 OK";
         let length = contents.len();
 
@@ -37,8 +51,33 @@ fn handle_connection(mut stream: TcpStream) {
             "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"     
         );
         buf_writer.write_all(response.as_bytes()).unwrap();
+    } else if request_line == "GET /main.js HTTP/1.1"   {
+        let contents = fs::read_to_string("./src/main.js").unwrap();
+        let status_line = "HTTP/1.1 200 OK";
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\nContent-Type: text/javascript\r\n\r\n{contents}"
+        );
+        buf_writer.write_all(response.as_bytes()).unwrap();
+    } else if request_line_split[0] == "PUT" {
+        let file_path = request_line_split[1].replace("%20", " ");
+
+        println!("{}", file_path);
+        let _ = fs::write(format!("{}{}", db_root, file_path), "some text"); 
+
+        let location = format!("{}{}", url_root, file_path);
+        let contents = fs::read_to_string(format!("{}{}", db_root, file_path)).unwrap();
+        let status_line = "HTTP/1.1 201 CREATED";
+        let length = contents.len();
+
+        let response = format!(
+            "{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}"
+        );
+
+        buf_writer.write_all(response.as_bytes()).unwrap();
     } else {
-        let contents = fs::read_to_string("./src/html/404.html").unwrap();
+        let contents = fs::read_to_string(format!("{}{}", ws_root, "/404.html")).unwrap();
         let status_line = "HTTP/1.1 404 NOT FOUND";
         let length = contents.len();
 
